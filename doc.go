218355008172ff26dfb20a79c662d7ab812f0c1e
Achille Roussel <achille@segment.com>
2016-12-05 18:28:10 -0800

    add doc

---
+++ doc.go
@@ -0,0 +1,4 @@
+// The conf package provides tools for easily loading program configurations
+// from multiple sources such as the command line arguments, environment, or
+// a configuration file.
+package conf
