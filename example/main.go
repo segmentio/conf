77876e90ae8dda138a31447acdb878ac073daa3d
Achille Roussel <achille@segment.com>
2017-01-03 22:57:33 -0800

    [skip ci] add example

---
+++ example/main.go
@@ -0,0 +1,37 @@
+// This example program can be used to test the features provided by the
+// github.com/segmentio/conf package.
+//
+// Passing configuration via the program arguments:
+//
+//	$ go run ./example/main.go -msg 'hello! (from the arguments)'
+//	[main] hello! (from the arguments)
+//
+// Passing configuration via the environment variables:
+//
+//	$ MAIN_MSG='hello! (from the environment)' go run ./example/main.go
+//	[main] hello! (from the environment)
+//
+// Passing configuration via a configuration file:
+//
+//	$ go run ./example/main.go -config-file ./example/config.yml
+//	[main] hello! (from the config file)
+//
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/segmentio/conf"
+)
+
+func main() {
+	config := struct {
+		Message string `conf:"msg" help:"The message to print out."`
+	}{
+		Message: "default",
+	}
+	conf.Load(&config)
+	fmt.Printf("[%s] %s\n", filepath.Base(os.Args[0]), config.Message)
+}
