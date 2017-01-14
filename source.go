cccaff328ad6c2bb84dc49745b815744431fbd78
Achille Roussel <achille@segment.com>
2017-01-13 16:21:16 -0800

    [skip ci] add docs

--- source.go
+++ source.go
@@ -8,26 +8,47 @@ import (
 	"strings"
 )
 
+// Source is the interface that allow new types to be plugged into a loader to
+// make it possible to load configuration from new places.
+//
+// When the configuration is loaded the Load method of each source that was set
+// on a loader is called with a pointer to an in-memory structure representing
+// the configuration object passed as dst.
 type Source interface {
 	Load(dst interface{}) error
 }
 
+// FlagSource is a special case of a source that receives a configuration value
+// from the arguments of a loader. It makes it possible to provide runtime
+// configuration to the source from the command line arguments of a program.
 type FlagSource interface {
 	Source
 
+	// Flag is the name of the flag that sets the source's configuration value.
 	Flag() string
 
+	// Help is called to get the help message to display for the source's flag.
 	Help() string
 
+	// flag.Value must be implemented by a FlagSource to receive their value
+	// when the loader's arguments are parsed.
 	flag.Value
 }
 
+// SourceFunc makes it possible to use basic function types as configuration
+// sources.
 type SourceFunc func(dst interface{}) error
 
+// Load calls f.
 func (f SourceFunc) Load(dst interface{}) error {
 	return f(dst)
 }
 
+// NewEnvSource creates a new source which loads values from the environment
+// variables given in env.
+//
+// A prefix may be set to namespace the environment variables that the source
+// will be looking at.
 func NewEnvSource(prefix string, env ...string) Source {
 	return SourceFunc(func(dst interface{}) (err error) {
 		if len(env) != 0 {
@@ -59,7 +80,21 @@ func NewEnvSource(prefix string, env ...string) Source {
 	})
 }
 
-func NewFileSource(flag string, vars interface{}, readFile func(string) ([]byte, error), unmarshal func([]byte, interface{}) error) Source {
+// NewFileSource creates a new source which loads a configuration from a file
+// identified by a path (or URL).
+//
+// The returned source satisfies the FlagSource interface because it loads the
+// file location from the given flag.
+//
+// The vars argument may be set to render the configuration file if it's a
+// template.
+//
+// The readFile function loads the file content in-memory from a file location
+// given as argument, usually this is ioutil.ReadFile.
+//
+// The unmarshal function decodes the content of the configuration file into a
+// configuration object.
+func NewFileSource(flag string, vars interface{}, readFile func(string) ([]byte, error), unmarshal func([]byte, interface{}) error) FlagSource {
 	return &fileSource{
 		flag:      flag,
 		vars:      vars,
