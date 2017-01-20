56b312b295ed33f0a527d50878f9badddff86b10
Maxence Charriere <charriere@outlook.com>
2017-01-19 16:29:22 -0800

    save implementation
    Save is a func to write a conf struct into a io.Writer.
    @achille-roussel
    Do you have any hints about how can I test if this output works?

---
+++ save.go
@@ -0,0 +1,135 @@
+package conf
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strings"
+)
+
+func SaveTo(name string, cfg interface{}) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	Save(f, cfg)
+	return nil
+}
+
+func Save(w io.Writer, cfg interface{}) {
+	v := reflect.ValueOf(cfg)
+	if v.Kind() != reflect.Struct {
+		panic(fmt.Sprint("cfg should be a struct"))
+	}
+
+	saveStruct(w, v, 0)
+}
+
+func saveStruct(w io.Writer, v reflect.Value, indent int) {
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		fv := v.Field(i)
+
+		conf := f.Tag.Get("conf")
+		if conf == "-" {
+			continue
+		}
+
+		confArgs := strings.Split(conf, ",")
+		if len(confArgs) >= 2 && confArgs[1] == "omitempty" {
+			fi := fv.Interface()
+			zi := reflect.Zero(f.Type).Interface()
+			if reflect.DeepEqual(fi, zi) {
+				continue
+			}
+		}
+
+		if help := f.Tag.Get("help"); len(help) != 0 {
+			fmt.Fprintln(w)
+			saveIndent(w, indent)
+			fmt.Fprintln(w, "#", help)
+		}
+
+		name := f.Name
+		if len(confArgs) > 0 && len(confArgs[0]) != 0 {
+			name = confArgs[0]
+		}
+
+		saveIndent(w, indent)
+		fmt.Fprintf(w, "%v: ", name)
+		save(w, fv, indent)
+	}
+}
+
+func save(w io.Writer, v reflect.Value, indent int) {
+	if _, ok := v.Type().MethodByName("String"); ok {
+		saveString(w, v, indent)
+		return
+	}
+
+	switch v.Kind() {
+	case reflect.Struct:
+		fmt.Fprintln(w)
+		saveStruct(w, v, indent+1)
+
+	case reflect.Map:
+		fmt.Fprintln(w)
+		saveMap(w, v, indent+1)
+
+	case reflect.Slice:
+		fmt.Fprintln(w)
+		saveSlice(w, v, indent+1)
+
+	case reflect.Ptr:
+		save(w, v.Elem(), indent)
+
+	default:
+		saveString(w, v, indent)
+	}
+}
+
+func saveMap(w io.Writer, v reflect.Value, indent int) {
+	for _, mk := range v.MapKeys() {
+		mv := v.MapIndex(mk)
+		saveIndent(w, indent)
+		fmt.Fprintf(w, "%v: ", mk.Interface())
+		save(w, mv, indent)
+	}
+}
+
+func saveSlice(w io.Writer, v reflect.Value, indent int) {
+	for i := 0; i < v.Len(); i++ {
+		sv := v.Index(i)
+		saveIndent(w, indent)
+		fmt.Fprint(w, "- ")
+		save(w, sv, indent)
+	}
+}
+
+func saveString(w io.Writer, v reflect.Value, indent int) {
+	str := fmt.Sprint(v.Interface())
+
+	s := strings.Split(str, "\n")
+	if len(s) == 1 {
+		fmt.Fprintln(w, str)
+		return
+	}
+
+	fmt.Fprintln(w, "|")
+	indent++
+
+	for _, line := range s {
+		saveIndent(w, indent)
+		fmt.Fprintln(w, line)
+	}
+}
+
+func saveIndent(w io.Writer, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Fprint(w, "  ")
+	}
+}
---
+++ save_test.go
@@ -0,0 +1,122 @@
+package conf
+
+import (
+	"bytes"
+	"os"
+	"testing"
+	"time"
+)
+
+type CfgSave struct {
+	String         string                       `conf:"string"                help:"A string"`
+	Int            int                          `conf:"int"                   help:"An int"`
+	OmitString     string                       `conf:"omit-string,omitempty" help:"Omit a string"`
+	OmitInt        int                          `conf:"omit-int,omitempty"    help:"Omit an int"`
+	Ignored        int                          `conf:"-"                     help:"Ignored field"`
+	Date           time.Time                    `conf:"date"                  help:"A date"`
+	SubStruct      CfgSub                       `conf:"sub-struct"            help:"A sub struct"`
+	Map            map[string]string            `conf:"map"                   help:"A map[string]string"`
+	MapStruct      map[int]CfgSub               `conf:"map-struct"            help:"A map[int]CfgSub"`
+	MapMap         map[string]map[string]string `conf:"map-map"               help:"A map[string]map[string]string"`
+	MapSlice       map[string][]int             `conf:"map-slice"             help:"A map[string][]int"`
+	SliceString    []string                     `conf:"slice-string"          help:"A slice of string"`
+	SliceStruct    []CfgSub                     `conf:"slice-struct"          help:"A slice of CfgSub"`
+	SliceMap       []map[string]string          `conf:"slice-map"             help:"A slice of map"`
+	SliceSlice     [][]string                   `conf:"slice-slice"           help:"A slice of slice"`
+	StructPtr      *CfgSub                      `conf:"struct-ptr"            help:"A ptr to CfgSub"`
+	Bool           bool                         `conf:"bool"                  help:"A boolean"`
+	MutilineString string                       `conf:"multi-line-string"     help:"A string with multiple lines"`
+}
+
+type CfgSub struct {
+	Name string
+	Age  int
+}
+
+func TestSave(t *testing.T) {
+	w := &bytes.Buffer{}
+
+	mline := `Hello world!
+I'm a gopher and this text is
+for multi line test...
+    `
+
+	cfg := CfgSave{
+		String:  "Hello world",
+		Int:     42,
+		Ignored: 777,
+		Date:    time.Now(),
+		SubStruct: CfgSub{
+			Name: "Jonhy",
+			Age:  42,
+		},
+		Map: map[string]string{
+			"Hello": "world",
+			"Foo":   "bar",
+		},
+		MapStruct: map[int]CfgSub{
+			1: CfgSub{
+				Name: "Max",
+				Age:  30,
+			},
+			2: CfgSub{
+				Name: "Ach",
+				Age:  29,
+			},
+		},
+		MapMap: map[string]map[string]string{
+			"magic": map[string]string{
+				"foo": "bar",
+				"ala": "kazam",
+			},
+			"food": map[string]string{
+				"apple": "pie",
+				"moji":  "to",
+			},
+		},
+		MapSlice: map[string][]int{
+			"geek": []int{42, 4242},
+			"luck": []int{777, 13},
+		},
+		SliceString: []string{
+			"Go",
+			"C++",
+			"C#",
+			"Java",
+		},
+		SliceStruct: []CfgSub{
+			CfgSub{
+				Name: "Vader",
+				Age:  42,
+			},
+			CfgSub{
+				Name: "Sidious",
+				Age:  84,
+			},
+		},
+		SliceMap: []map[string]string{
+			map[string]string{
+				"foo": "bar",
+				"ala": "kazam",
+			},
+			map[string]string{
+				"apple": "pie",
+				"moji":  "to",
+			},
+		},
+		SliceSlice: [][]string{
+			[]string{"tonkatsu", "yakiniku"},
+			[]string{"panang", "masaman"},
+		},
+		StructPtr: &CfgSub{
+			Name: "Hamtaro",
+			Age:  2,
+		},
+		MutilineString: mline,
+	}
+
+	Save(w, cfg)
+	t.Log(w.String())
+
+	ld := defaultLoader(os.Args, os.Environ())
+}
