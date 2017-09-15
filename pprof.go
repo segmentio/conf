6cc4bafdf89753fd6cdf8ab876d1af7808101861
Achille Roussel <achille@segment.com>
2017-09-15 09:27:49 -0700

    add conf.PPROF

---
+++ pprof.go
@@ -0,0 +1,25 @@
+package conf
+
+import "runtime"
+
+// PPROF is a confiuration struct which can be used to configure the runtime
+// profilers of programs.
+type PPROF struct {
+	BlockProfileRate     float64 `conf:"block-profile-rate"     help:"Sets the mutex profile fraction to enable runtime profiling of lock contention, zero disables mutex profiling" validate:"min=0,max=1"`
+	MutexProfileFraction float64 `conf:"mutex-profile-fraction" help:"Sets the mutex profile fraction to enable runtime profiling of lock contention, zero disables mutex profiling" validate:"min=0,max=1"`
+}
+
+// DefaultPPROF returns the default value of a PPROF struct. Note that the
+// zero-value is valid, DefaultPPROF differs because it captures the current
+// configuration of the program's runtime.
+func DefaultPPROF() PPROF {
+	return PPROF{
+		MutexProfileFraction: 1 / float64(runtime.SetMutexProfileFraction(-1)),
+	}
+}
+
+// SetPPROF configures the runtime profilers based on the given PPROF config.
+func SetPPROF(config PPROF) {
+	runtime.SetBlockProfileRate(int(1 / config.BlockProfileRate))
+	runtime.SetMutexProfileFraction(int(1 / config.MutexProfileFraction))
+}
