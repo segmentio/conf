bbd9d230dae721b94dd487022ad4bc5cdf7e9571
Achille Roussel <achille@segment.com>
2017-09-15 09:30:12 -0700

    cleanup

--- pprof.go
+++ pprof.go
@@ -12,8 +12,8 @@ import "runtime"
 //	conf.SetPPROF(config.PPROF)
 //
 type PPROF struct {
-	BlockProfileRate     float64 `conf:"block-profile-rate"     help:"Sets the mutex profile fraction to enable runtime profiling of lock contention, zero disables mutex profiling" validate:"min=0,max=1"`
-	MutexProfileFraction float64 `conf:"mutex-profile-fraction" help:"Sets the mutex profile fraction to enable runtime profiling of lock contention, zero disables mutex profiling" validate:"min=0,max=1"`
+	BlockProfileRate     int `conf:"block-profile-rate"     help:"Sets the mutex profile fraction to enable runtime profiling of lock contention, zero disables mutex profiling" validate:"min=0,max=1"`
+	MutexProfileFraction int `conf:"mutex-profile-fraction" help:"Sets the mutex profile fraction to enable runtime profiling of lock contention, zero disables mutex profiling" validate:"min=0,max=1"`
 }
 
 // DefaultPPROF returns the default value of a PPROF struct. Note that the
@@ -21,12 +21,12 @@ type PPROF struct {
 // configuration of the program's runtime.
 func DefaultPPROF() PPROF {
 	return PPROF{
-		MutexProfileFraction: 1 / float64(runtime.SetMutexProfileFraction(-1)),
+		MutexProfileFraction: runtime.SetMutexProfileFraction(-1),
 	}
 }
 
 // SetPPROF configures the runtime profilers based on the given PPROF config.
 func SetPPROF(config PPROF) {
-	runtime.SetBlockProfileRate(int(1 / config.BlockProfileRate))
-	runtime.SetMutexProfileFraction(int(1 / config.MutexProfileFraction))
+	runtime.SetBlockProfileRate(config.BlockProfileRate)
+	runtime.SetMutexProfileFraction(config.MutexProfileFraction)
 }
