6f4351c58bb2a85a48e9f4999def7f2931542f77
Kevin Burke <kevin.burke@segment.com>
2022-07-21 20:37:43 -0700

    node_test: fix for use with yaml
    This yaml.Unmarshal call unmarshals dates with quotes, so let's change
    the test representation to match.

--- node_test.go
+++ node_test.go
@@ -341,7 +341,7 @@ func TestNodeString(t *testing.T) {
 			node: Scalar{reflect.ValueOf("Hello World!")},
 		},
 		{
-			repr: `2016-12-31T23:42:59Z`,
+			repr: `"2016-12-31T23:42:59Z"`,
 			node: Scalar{reflect.ValueOf(date)},
 		},
 		{
@@ -374,8 +374,8 @@ func TestNodeString(t *testing.T) {
 		t.Run(test.repr, func(t *testing.T) {
 			if repr := test.node.String(); repr != test.repr {
 				t.Error("representation mismatch")
-				t.Log("expected:", test.repr)
-				t.Log("found:   ", repr)
+				t.Logf("want: %v, %#v", reflect.TypeOf(test.repr), test.repr)
+				t.Logf("got: %v, %#v", reflect.TypeOf(repr), repr)
 			}
 		})
 	}
